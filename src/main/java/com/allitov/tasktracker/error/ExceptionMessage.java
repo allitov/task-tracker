fa1d1411cd9f754dbf87a3635948747a9ef98fd2
allitov <litovkinalexan@gmail.com>
2024-02-12 16:14:32 +0700

    Implement user service

---
+++ src/main/java/com/allitov/tasktracker/error/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package com.allitov.tasktracker.error;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExceptionMessage {
+
+    public final String USER_BY_ID_NOT_FOUND = "User with id '%s' not found";
+}
---
+++ src/main/java/com/allitov/tasktracker/model/service/impl/DatabaseUserService.java
@@ -0,0 +1,51 @@
+package com.allitov.tasktracker.model.service.impl;
+
+import com.allitov.tasktracker.error.EntityNotFoundException;
+import com.allitov.tasktracker.error.ExceptionMessage;
+import com.allitov.tasktracker.model.entity.User;
+import com.allitov.tasktracker.model.repository.UserRepository;
+import com.allitov.tasktracker.model.service.UserService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class DatabaseUserService implements UserService<User, String> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Flux<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Mono<User> findById(@NonNull String id) {
+        return userRepository.findById(id)
+                .switchIfEmpty(
+                        Mono.error(
+                                new EntityNotFoundException(String.format(ExceptionMessage.USER_BY_ID_NOT_FOUND, id))
+                        )
+                );
+    }
+
+    @Override
+    public Mono<User> create(@NonNull User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Mono<User> update(@NonNull User user) {
+        return findById(user.getId()).flatMap(
+                foundUser -> !foundUser.equals(user) ? userRepository.save(user) : Mono.just(foundUser)
+        );
+    }
+
+    @Override
+    public Mono<Void> deleteById(@NonNull String id) {
+        return userRepository.deleteById(id);
+    }
+}
