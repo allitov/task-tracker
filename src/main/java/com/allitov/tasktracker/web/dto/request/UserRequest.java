40729debe4100c294747ac7a19417b4c9baf3c43
allitov <litovkinalexan@gmail.com>
2024-02-12 17:46:03 +0700

    Add mapstruct

---
+++ src/main/java/com/allitov/tasktracker/web/dto/request/UserRequest.java
@@ -0,0 +1,15 @@
+package com.allitov.tasktracker.web.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+
+    private String username;
+
+    private String email;
+}
---
+++ src/main/java/com/allitov/tasktracker/web/dto/response/UserListResponse.java
@@ -0,0 +1,16 @@
+package com.allitov.tasktracker.web.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserListResponse {
+
+    private List<UserResponse> users = new ArrayList<>();
+}
