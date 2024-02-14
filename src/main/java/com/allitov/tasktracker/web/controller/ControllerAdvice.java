54aa9311261070c611b11cb2e439df571cbfaddc
allitov <litovkinalexan@gmail.com>
2024-02-14 17:56:31 +0700

    Change ControllerAdvice.java

--- src/main/java/com/allitov/tasktracker/web/controller/ControllerAdvice.java
+++ src/main/java/com/allitov/tasktracker/web/controller/ControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.allitov.tasktracker.web.controller;
 
 import com.allitov.tasktracker.error.EntityNotFoundException;
+import com.allitov.tasktracker.web.dto.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +12,7 @@ import reactor.core.publisher.Mono;
 public class ControllerAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public Mono<ResponseEntity<Void>> EntityNotFoundExceptionHandler() {
-        return Mono.just(ResponseEntity.notFound().build());
+    public Mono<ResponseEntity<ErrorResponse>> EntityNotFoundExceptionHandler(EntityNotFoundException e) {
+        return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage())));
     }
 }
